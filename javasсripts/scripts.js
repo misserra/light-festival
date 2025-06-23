6dfe12b623a7d023afffc54ddf8dbb64d90c1a99
misserra <misserra17@gmail.com>
2025-06-23 22:18:07 +0300

    осталось ещё примерно миллион

--- javasсripts/scripts.js
+++ javasсripts/scripts.js
@@ -11,4 +11,19 @@ document.addEventListener('DOMContentLoaded', function() {
   } else {
     console.error('Элемент .scroll-down не найден');
   }
+  const burgerIcon = document.getElementById('burgerIcon');
+  const menuLinks = document.getElementById('menuLinks');
+  const menuContainer = document.querySelector('.menu-container');
+  
+  burgerIcon.addEventListener('click', function() {
+    menuContainer.classList.toggle('menu-open');
+  });
+  
+  menuLinks.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', function() {
+      menuContainer.classList.remove('menu-open');
+    });
+  });
+  
 });
+
